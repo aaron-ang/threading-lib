c2f8f07ce5665470207570ada5ea0f1c32a81010
aaron-ang <67321817+aaron-ang@users.noreply.github.com>
2024-02-14 23:39:36 -0500

    fix tests

--- test_busy_threads.c
+++ test_busy_threads.c
@@ -1,6 +1,5 @@
 #include <assert.h>
 #include <pthread.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
--- test_custom_schedule.c
+++ test_custom_schedule.c
@@ -6,13 +6,26 @@
 
 void *hello(void *arg) {
   printf("Hello from thread %ld\n", pthread_self());
+  pthread_exit(&arg);
   return NULL;
 }
 
 int main() {
   pthread_t threads[THREAD_CNT];
-  unsigned long int i;
+  unsigned long i;
   for (i = 0; i < THREAD_CNT; i++) {
     pthread_create(&threads[i], NULL, hello, (void *)i);
+  }
+
+  // run all threads
+  schedule(0);
+
+  // wait for all threads to finish
+  for (i = 0; i < THREAD_CNT; i++) {
+    void *pret;
+    int ret;
+    pthread_join(threads[i], &pret);
+    ret = *(unsigned long *)pret;
+    assert(ret == i);
   }
 }
