59d2e2141c8d9e30373638f7715d331847dc2a71
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2024-04-12 19:57:36 -0400

    implement synch

--- threads.c
+++ threads.c
@@ -1,4 +1,5 @@
 #include "ec440threads.h"
+#include <errno.h>
 #include <pthread.h>
 #include <signal.h>
 #include <stdbool.h>
@@ -14,10 +15,36 @@
 #define THREAD_STACK_SIZE (1 << 15) /* size of stack in bytes */
 #define QUANTUM (50 * 1000)         /* quantum in usec */
 
+#ifndef PTHREAD_BARRIER_SERIAL_THREAD
+#define PTHREAD_BARRIER_SERIAL_THREAD 1
+#endif
+
+typedef struct {
+  int flag;
+} lock_t;
+
+typedef union {
+  pthread_mutex_t mutex;
+  lock_t my_mutex;
+} my_mutex_t;
+
+typedef struct {
+  pthread_mutex_t mutex;
+  pthread_cond_t cond;
+  unsigned limit;
+  unsigned count;
+  unsigned phase;
+} barrier_t;
+
+typedef union {
+  pthread_barrier_t barrier;
+  barrier_t my_barrier;
+} my_barrier_t;
+
 enum thread_status { TS_EXITED, TS_RUNNING, TS_READY };
 
 typedef struct thread_control_block {
-  int id;
+  pthread_t id;
   jmp_buf registers;
   void *stack;
   enum thread_status status;
@@ -31,6 +58,10 @@ int num_threads = 0;
 static void scheduler_init();
 
 static void init_handler();
+
+static void lock();
+
+static void unlock();
 
 static TCB *get_new_thread();
 
@@ -83,15 +114,27 @@ void init_handler() {
   ualarm(QUANTUM, QUANTUM);
 }
 
+void lock() { assert(sigprocmask(SIG_BLOCK, &(sigset_t){SIGALRM}, NULL) == 0); }
+
+void unlock() {
+  assert(sigprocmask(SIG_UNBLOCK, &(sigset_t){SIGALRM}, NULL) == 0);
+}
+
+int test_and_set(int *lock) {
+  int ret = *lock;
+  *lock = 1;
+  return ret;
+}
+
 TCB *get_new_thread() {
-  static int i = 0;
+  static long i = 0;
   int seen = 0;
   while (threads[i].status != TS_EXITED || threads[i].stack) {
     if (++seen >= MAX_THREADS)
       exit(EXIT_FAILURE);
     i = (i + 1) % MAX_THREADS;
   }
-  threads[i].id = i;
+  threads[i].id = (pthread_t)i;
   return &threads[i];
 }
 
@@ -159,12 +202,77 @@ void schedule(int signal) {
 
   current_thread = i;
   threads[current_thread].status = TS_RUNNING;
-  longjmp(threads[current_thread].registers, threads[current_thread].id + 1);
+  longjmp(threads[current_thread].registers,
+          (long)threads[current_thread].id + 1);
 }
 
-/*
- * Don't implement main in this file!
- * This is a library of functions, not an executable program. If you
- * want to run the functions in this file, create separate test programs
- * that have their own main functions.
- */
+int pthread_mutex_init(pthread_mutex_t *mutex,
+                       const pthread_mutexattr_t *attr) {
+  my_mutex_t *m = (my_mutex_t *)mutex;
+  m->my_mutex.flag = 0;
+  return 0;
+}
+
+int pthread_mutex_destroy(pthread_mutex_t *mutex) { return 0; }
+
+int pthread_mutex_lock(pthread_mutex_t *mutex) {
+  lock();
+  my_mutex_t *m = (my_mutex_t *)mutex;
+  while (test_and_set(&m->my_mutex.flag)) {
+    schedule(0);
+  }
+  m->my_mutex.flag = 1;
+  unlock();
+  return 0;
+}
+
+int pthread_mutex_unlock(pthread_mutex_t *mutex) {
+  my_mutex_t *m = (my_mutex_t *)mutex;
+  m->my_mutex.flag = 0;
+  return 0;
+}
+
+int pthread_barrier_init(pthread_barrier_t *restrict barrier,
+                         const pthread_barrierattr_t *attr, unsigned count) {
+  if (count == 0)
+    return EINVAL;
+
+  my_barrier_t *b = (my_barrier_t *)barrier;
+  if (pthread_mutex_init(&b->my_barrier.mutex, NULL))
+    return -1;
+  if (pthread_cond_init(&b->my_barrier.cond, NULL)) {
+    pthread_mutex_destroy(&b->my_barrier.mutex);
+    return -1;
+  }
+  b->my_barrier.limit = count;
+  b->my_barrier.count = 0;
+  b->my_barrier.phase = 0;
+  return 0;
+}
+
+int pthread_barrier_destroy(pthread_barrier_t *barrier) {
+  my_barrier_t *b = (my_barrier_t *)barrier;
+  pthread_mutex_destroy(&b->my_barrier.mutex);
+  pthread_cond_destroy(&b->my_barrier.cond);
+  return 0;
+}
+
+int pthread_barrier_wait(pthread_barrier_t *barrier) {
+  my_barrier_t *b = (my_barrier_t *)barrier;
+  pthread_mutex_lock(&b->my_barrier.mutex);
+  b->my_barrier.count++;
+  if (b->my_barrier.count >= b->my_barrier.limit) {
+    b->my_barrier.count = 0;
+    b->my_barrier.phase++;
+    pthread_cond_broadcast(&b->my_barrier.cond);
+    pthread_mutex_unlock(&b->my_barrier.mutex);
+    return PTHREAD_BARRIER_SERIAL_THREAD;
+  } else {
+    unsigned phase = b->my_barrier.phase;
+    do {
+      pthread_cond_wait(&b->my_barrier.cond, &b->my_barrier.mutex);
+    } while (phase == b->my_barrier.phase);
+    pthread_mutex_unlock(&b->my_barrier.mutex);
+    return 0;
+  }
+}
