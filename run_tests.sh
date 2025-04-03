d7d0a795b73427b529ed95a4be4224bcac1f0f20
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2025-04-02 23:53:05 -0700

    Add CI workflow

--- run_tests.sh
+++ run_tests.sh
@@ -6,8 +6,7 @@ test_count=$#
 fail_count=0
 failed_tests=""
 
-for test_file in $all_tests
-do
+for test_file in $all_tests; do
 	echo "===== ${test_file} ====="
 	rm -f testfs # Tidy up from previous tests
 	timeout ${TIMEOUT_SECONDS} ${test_file}
@@ -15,19 +14,19 @@ do
 	if [ ${rc} -eq 0 ]; then
 		echo "PASS"
 	else
-    	if [ ${rc} -eq 124 ]; then
-    		echo "FAIL (${TIMEOUT_SECONDS} second timeout)"
-    	else
-    		echo "FAIL (rc = ${rc})"
-        fi
-        failed_tests+="${test_file}:(rc=${rc}) "
-        fail_count=$((fail_count + 1))
+		if [ ${rc} -eq 124 ]; then
+			echo "FAIL (${TIMEOUT_SECONDS} second timeout)"
+		else
+			echo "FAIL (rc = ${rc})"
+		fi
+		failed_tests+="${test_file}:(rc=${rc}) "
+		fail_count=$((fail_count + 1))
 	fi
 done
 
 echo "${fail_count} out of ${test_count} tests failed."
 
-if [ ${fail_count} -gt 0 ]
-then
-    echo "Failed tests: ${failed_tests}"
+if [ ${fail_count} -gt 0 ]; then
+	echo "Failed tests: ${failed_tests}"
+	exit 1
 fi
